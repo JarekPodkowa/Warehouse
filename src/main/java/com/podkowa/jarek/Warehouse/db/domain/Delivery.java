c58bace809eb021171498c370ff54d1773dda1c5
Jarek <jarekm4a1@interia.pl>
2019-01-05 17:07:40 +0100

    Add models

---
+++ src/main/java/com/podkowa/jarek/Warehouse/db/domain/Delivery.java
@@ -0,0 +1,40 @@
+package com.podkowa.jarek.Warehouse.db.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.time.Instant;
+
+@Entity
+@Data
+@Table(name = "delivery")
+@AllArgsConstructor
+public class Delivery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "product_id",
+            foreignKey = @ForeignKey(name = "FK_product_product_id_delivery")
+    )
+    private Product product;
+
+    @CreationTimestamp
+    @Column(name = "record_created_on")
+    private Instant recordCreatedOn;
+}
